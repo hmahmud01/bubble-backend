e805bed96477363cc66510c2940f3e0183861cdd
hmahmud01 <hmahmud01@gmail.com>
2022-07-28 03:49:48 +0600

    cors added

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,7 +1,11 @@
 'use strict';
 
-exports.modifierWord = function(req, res){
+exports.modifierWord = function(req, res){    
     var data_get = req.body;
+
+    console.log(data_get);
+
+    console.log(data_get.word);
 
     var input = data_get.word;
     var eraser = data_get.remover;
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express');
+const cors = require('cors');
 var app = express();
+app.use(cors());
+
 var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
+
 
 var routes = require('./api/routes/index.js');
 routes(app);
